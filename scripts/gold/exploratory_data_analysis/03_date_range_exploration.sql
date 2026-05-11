fbec9de2befbcbdf65e8ad3186582f8bee0d875a
MariorData <mromerom80@gmail.com>
2026-05-11 14:48:04 +0200

    Create 03_date_range_exploration.sql

---
+++ scripts/gold/exploratory_data_analysis/03_date_range_exploration.sql
@@ -0,0 +1,25 @@
+/*
+===============================================================================
+Date Range Exploration 
+===============================================================================
+Purpose:
+    - To determine the temporal boundaries of key data points.
+    - To understand the range of historical data.
+
+SQL Functions Used:
+    - MIN(), MAX(), DATEDIFF()
+===============================================================================
+*/
+
+-- Retrieve the first and last order date and the total duration in months
+SELECT
+    MIN(order_date) AS first_order_date,
+    MAX(order_date) AS last_order_date,
+    DATEDIFF(MONTH,MIN(order_date),MAX(order_date)) AS order_range_months
+FROM gold.fact_sales
+
+-- Find first and last join date from customers
+SELECT 
+    MIN(join_date) AS first_order_date,
+    MAX(join_date) AS last_order_date
+FROM gold.dim_customer
