fa22d736fbc97318e5cf47a4d124619956b4233d
MariorData <mromerom80@gmail.com>
2026-05-11 22:05:53 +0200

    Update 04_measures_exploration.sql

--- scripts/gold/exploratory_data_analysis/04_measures_exploration.sql
+++ scripts/gold/exploratory_data_analysis/04_measures_exploration.sql
@@ -35,3 +35,52 @@ WITH kg_cal AS (
 SELECT
 SUM(kg) AS total_kg
 FROM kg_cal
+
+--  Find the average selling price
+
+SELECT
+AVG(unit_price) AS avg_gross_price,
+SUM(revenue)/SUM(quantity) AS avg_net_price
+FROM gold.fact_sales
+
+-- Find the Total number of Orders
+SELECT
+COUNT(order_id) AS total_orders
+FROM gold.fact_sales
+SELECT
+COUNT(DISTINCT order_id) AS total_orders
+FROM gold.fact_sales
+
+-- Find the total number of products
+SELECT
+COUNT(DISTINCT product_code)
+FROM gold.dim_product
+
+-- Find the total number of products sold
+SELECT
+COUNT(DISTINCT product_code)
+FROM gold.fact_sales
+
+-- Find the total number of customers
+SELECT
+COUNT(DISTINCT customer_code)
+FROM gold.dim_customer
+
+-- Find the total number of customers that has placed an order
+SELECT
+COUNT(DISTINCT customer_code)
+FROM gold.fact_sales
+
+-- Report with all key business metrics
+
+SELECT 'Total Sales' AS measure_name, SUM(revenue) AS measure_value FROM gold.fact_sales
+UNION ALL
+SELECT 'Total Quantity', SUM(quantity) FROM gold.fact_sales
+UNION ALL
+SELECT 'Average Net Price', SUM(revenue)/SUM(quantity) FROM gold.fact_sales
+UNION ALL
+SELECT 'Total Orders', COUNT(DISTINCT order_id) FROM gold.fact_sales
+UNION ALL
+SELECT 'Total Products', COUNT(DISTINCT product_code) FROM gold.dim_product
+UNION ALL
+SELECT 'Total Customers', COUNT(customer_code) FROM gold.dim_customer;
